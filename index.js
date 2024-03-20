f0a8b56654d6980d05435f16c89933d2c9e1abf8
marcelitos1v9 <marceloaugustocge@gmail.com>
2024-03-20 20:20:45 -0300

    modified:   index.js

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ app.get("/",(req, res) => {
 
 //rota perfil
 
-app.get("/perfil/:nome",(req, res) => {
+app.get("/perfil/:nome?",(req, res) => {
     //:nome -> parametro obrigatório  
     //coletando o parametro da rota e gravando na variavel nome
     const nome = req.params.nome
-    res.send(`<h1>Ola ${nome} seja bem vindo a seu perfil!<//h1>`)
+    if(nome) {
+        res.send(`<h1>Ola ${nome} seja bem vindo a seu perfil!</h1>`)
+    }else{
+        res.send(`<h1 style='text-align=center;'>Faça seu login agora!</h1>`)
+    }
+    
 })
 
 //rota videos 
@@ -26,10 +31,21 @@ app.get("/perfil/:nome",(req, res) => {
 app.get("/videos/:playlist/:video",(req, res) => {
     const play = req.params.playlist
     const video = req.params.video
-    
-    res.send("<h1>Videos do usuário!</h1>");
+
+    res.send(`<h2 style='text-align:center;'>Você esta na playlist de ${play} <br> Reproduzindo o vídeo <strong> ${video}...</strong></h2>`);
 
 })
+
+app.get("/produtos/:produto?",(req, res)=>{
+    const produto = req.params.produto
+    
+    if (produto) {
+        res.send(`<h2> ${produto}</h2>`)
+    }else{
+        res.send(`<h1>Usuario por favor insira o produto</h1>`)
+    }
+})
+
 
 
 app.listen(8080, function(erro){
